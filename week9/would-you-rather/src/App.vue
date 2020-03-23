8ceb84407694e3f8d4e3c93ce62c08504221352f
brndn adms <hey@brandenadams.com>
2020-03-22 19:21:39 -0500

    finish lab

---
+++ week9/would-you-rather/src/App.vue
@@ -0,0 +1,99 @@
+<template>
+  <div id="app">
+    <WouldYouRather
+      v-for="element in questions"
+      v-bind:key="element.id"
+      v-bind:question="element"
+      v-on:answer-changed="answerChanged"
+    />
+    <div id="answers" v-if="answers.length">
+      <p>You would rather:</p>
+      <ul>
+        <li v-for="answer in answers" v-bind:key="answer.question">{{ answer.choice }}</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+import WouldYouRather from "./components/WouldYouRather.vue";
+
+export default {
+  name: "App",
+  components: {
+    WouldYouRather
+  },
+  data: function() {
+    return {
+      answers: [],
+      questions: [
+        {
+          id: 1,
+          question: "...live in a house shaped like a cicle or a triangle?",
+          answer1: "live in a circle",
+          answer2: "live in a triangle"
+        },
+        {
+          id: 2,
+          question: "...be a detective or a pilot?",
+          answer1: "be a detective",
+          answer2: "be a pilot"
+        },
+        {
+          id: 3,
+          question: "...go skiing or go to a water park?",
+          answer1: "go skiing",
+          answer2: "go to a water park"
+        },
+        {
+          id: 4,
+          question: "...fly a kite or swing on a swing?",
+          answer1: "fly a kite",
+          answer2: "swing"
+        },
+        {
+          id: 5,
+          question: "...dance or sing?",
+          answer1: "dance",
+          answer2: "sing"
+        },
+        {
+          id: 6,
+          question: "...play hide and seek or dodgeball?",
+          answer1: "play hide and seek",
+          answer2: "play dodgeball"
+        },
+        {
+          id: 7,
+          question: "...be incredibly funny or incredibly smart?",
+          answer1: "be funny",
+          answer2: "be smart"
+        }
+      ]
+    };
+  },
+  methods: {
+    answerChanged: function(question, choice) {
+      this.answers = this.answers.filter(answer => answer.id !== question.id);
+      this.answers.push({
+        id: question.id,
+        choice
+      });
+      this.answers.sort((a, b) => a.id - b.id);
+    }
+  }
+};
+</script>
+<style>
+body {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+  background-color:wheat;
+}
+#answers {
+  position: fixed;
+  right: 50px;
+  top: 0px;
+}
+</style>
---
+++ week9/would-you-rather/src/components/WouldYouRather.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="question">
+    <h2>{{ question.question }}</h2>
+    <input
+      type="radio"
+      v-model="choice"
+      v-bind:value="question.answer1"
+      v-on:change="$emit('answer-changed', question, choice)"
+    />
+    <label>{{ question.answer1 }}</label>
+    <input
+      type="radio"
+      v-model="choice"
+      v-bind:value="question.answer2"
+      v-on:change="$emit('answer-changed', question, choice)"
+    />
+    <label>{{ question.answer2 }}</label>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "WouldYouRather",
+  data() {
+    return {
+      choice: ""
+    };
+  },
+  props: {
+    question: Object
+  }
+};
+</script>
+
+<style>
+.question {
+  margin: 10px;
+  padding: 20px;
+  width: 500px;
+  background: rgb(255, 217, 210);
+  border: 1px solid rgb(250, 179, 166);
+  box-shadow: inset 1px 1px rgb(250, 179, 166);
+  box-shadow: 1px 1px tomato;
+  border-radius: 5px;
+}
+</style>
